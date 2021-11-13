1584d6161498bf04ad9a37f27877a36a75f720da
Felipe Fonseca Bremgartner <felipe.fonseca.bremgartner@gmail.com>
2021-11-13 15:26:47 -0400

    Update ftc.py
    codtransacao add

--- ftc.py
+++ ftc.py
@@ -45,6 +45,23 @@ def valida_formato_cpf(entrada):
     d2 = (sum(map(calc, enumerate(reversed(entrada[:-1])))) * 10) % 11
     return str(d1) == entrada[-2] and str(d2) == entrada[-1]
 
+def codtransacao (cod):
+    cod_transacao = cod.split("-")
+    match1 = re.search(r"[0-9]{9}",cod_transacao[0])
+    match2 = re.search(r"(?!([a-z0-9])*.*\1)([a-z0-9]){5}",cod_transacao[1])
+    match3 = re.search(r"([02468]){3}",cod_transacao[2])
+    try:
+        match4 = re.search(r"([01]){3}",cod_transacao[3])
+        if (match1 and match2 and match3 and match4):
+            return True
+        else:
+            return False
+    except:
+        if (match1 and match2 and match3):
+            return True
+        else:
+            return False
+
 cpf_regex  = "^\d{3}\.\d{3}\.\d{3}\-\d{2}$"
 
 

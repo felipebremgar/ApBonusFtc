febb5484c0428b639b8fb7013589609a93a46212
HermannHernani <hermannhernani@outlook.com>
2021-11-13 18:49:10 -0400

    Update ftc.py - added cnpj validation

--- ftc.py
+++ ftc.py
@@ -2,12 +2,13 @@
 # valido: 159.200.335-41 012.690.402-27 2011.11.11 17:18:30 [14.40,13.30,14.40,155.60] 658214793-ta8ux-822
 #         277.781.070-23 39.900.653/0001-62 2016.02.29 22:33:11 [3.00,4.00,12.00] 746985321-pof32-000-111
 # invalido: 573.294.590-000 260.382.860-63 2018.03.13 14:09:59 [600.00] 666333999-bcd45-246-000
+#           39.900.653/0001-62            
+
 import re
 cpf_regex  = "^\d{3}\.\d{3}\.\d{3}\-\d{2}$"
 data_regex = "([12]\d{3}.(0[1-9]|1[0-2]).(0[1-9]|[12]\d|3[01]))"
 hour_regex = "^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$"
-cnpj_regex = "^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}"
-
+cnpj_regex = "\d{2}\.?\d{3}\.?\d{3}\/?\d{4}\-?\d{2}"
 
 def trata_entrada(entrada):
     tratada = entrada.split(" ") 
@@ -48,10 +49,10 @@ def valida_formato_cpf(entrada):
     calc = lambda t: int(t[1]) * (t[0] + 2)
     d1 = (sum(map(calc, enumerate(reversed(entrada[:-2])))) * 10) % 11
     d2 = (sum(map(calc, enumerate(reversed(entrada[:-1])))) * 10) % 11
-    return str(d1) == entrada[-2] and str(d2) == entrada[-1] 
+    return str(d1) == entrada[-2] and str(d2) == entrada[-1]
 
-def valida_cod_transacao (entrada):
-    cod_transacao = entrada.split("-")
+def valida_cod_transacao (cod):
+    cod_transacao = cod.split("-")
     match1 = re.search(r"[0-9]{9}",cod_transacao[0])
     match2 = re.search(r"(?!([a-z0-9])*.*\1)([a-z0-9]){5}",cod_transacao[1])
     match3 = re.search(r"([02468]){3}",cod_transacao[2])
@@ -81,11 +82,81 @@ def valida_hora(entrada):
     validacao = re.match(hour_regex, entrada)
     if len(entrada) != 0:
         if validacao:
-            print ('True')
+            return ('True')
         else:
-            print ('False')
+            return ('False')
     else:
         print ('Error')
+
+def valida_formato_cnpj(entrada):
+    validacao = re.match(cnpj_regex, entrada)
+    if len(entrada) != 0:
+        if validacao:
+            return ('True')
+        else:
+            return ('False')
+    else:
+        print ('Error')
+
+def valida_cnpj(entrada):
+    validation = re.match(cnpj_regex,entrada)
+    cnpj = re.sub(r'[^0-9]', '', entrada)
+    if (valida_formato_cnpj != True):
+        raiz_cnpj = cnpj[0:12]
+        digito_ver = cnpj[12:14]
+
+        ver = int(raiz_cnpj[0])*5
+        ver += int(raiz_cnpj[1])*4
+        ver += int(raiz_cnpj[2])*3
+        ver += int(raiz_cnpj[3])*2
+        ver += int(raiz_cnpj[4])*9
+        ver += int(raiz_cnpj[5])*8
+        ver += int(raiz_cnpj[6])*7
+        ver += int(raiz_cnpj[7])*6
+        ver += int(raiz_cnpj[8])*5
+        ver += int(raiz_cnpj[9])*4
+        ver += int(raiz_cnpj[10])*3
+        ver += int(raiz_cnpj[11])*2
+
+        ver_resto = ver % 11
+
+        digito_1 = 0
+
+        if ver_resto < 2:
+            digito_1 = 0
+        else:
+            digito_1 = 11-ver_resto
+
+        ver = int(raiz_cnpj[0])*6
+        ver += int(raiz_cnpj[1])*5
+        ver += int(raiz_cnpj[2])*4
+        ver += int(raiz_cnpj[3])*3
+        ver += int(raiz_cnpj[4])*2
+        ver += int(raiz_cnpj[5])*9
+        ver += int(raiz_cnpj[6])*8
+        ver += int(raiz_cnpj[7])*7
+        ver += int(raiz_cnpj[8])*6
+        ver += int(raiz_cnpj[9])*5
+        ver += int(raiz_cnpj[10])*4
+        ver += int(raiz_cnpj[11])*3
+        ver += digito_1 * 2
+
+        ver_resto = ver % 11
+
+        digito_2 = 0
+
+        if ver_resto < 2:
+            digito_2 = 0
+        else:
+            digito_2 = 11-ver_resto
+            
+        digito_calc = str(digito_1) + str(digito_2)
+
+        if digito_calc == digito_ver:
+            print('True')
+        else:
+            print('False')
+    else: print ("False")
 
 def valida_preco(entrada):
     match = re.search(r"^\[([0-9]+.[0-9]{2})(,([0-9])+.([0-9]){2})*\]$", entrada)
@@ -95,8 +166,6 @@ def valida_preco(entrada):
     else:
         return False
 
+# def valida_cnpj(entrada):
 entrada = input()
-valida_hora(entrada)
-
-
-
+valida_cnpj(entrada)
